2cd6b7d6bb2fcfb68d28722e5f6cfbf7d30bcb93
Baraa <engbaraakhamaysa@gmail.com>
2025-09-09 19:06:42 +0300

    AJAX , Requeset & Response Files

---
+++ 176_AJAX&NetworkInformation.js
@@ -0,0 +1,19 @@
+/* 
+AJAX
+-- Asynchronous JavaScript And XML
+-- Approach To Use Many Techonlogies Together [HTML, CSS , JS , DOM]
+-- It Use "XMLHttpRequest" Object To Interact With The Server
+-- You Can Fetch Date Or Send Data Without Page Refresh
+- Examples
+-- Youtube Studio
+-- Google Drive
+-- Upload Artcle Photo
+-- Form Check Name
+
+Test new XMLHttpRequest();
+Request And Response
+Status Code
+*/
+
+let req = new XMLHttpRequest();
+console.log(req);
