58b319e4a41119ed2d3f6583dff41974f3278e90
Baraa <engbaraakhamaysa@gmail.com>
2025-09-11 23:57:38 +0300

    Promise: Then, Catch & Finally  File

--- 181_Promise_Then_Catch_Finally.js
+++ 181_Promise_Then_Catch_Finally.js
@@ -15,4 +15,4 @@ Finally => Promise Successfull Or Failed Finally Do Somethiing
 
 */
 const myPromise = new Promise((resolveFunction, rejectFunction) => {});
-myPromise.then();
+myPromise.then((resolveValue) => console.log(`${resolveValue}`));
