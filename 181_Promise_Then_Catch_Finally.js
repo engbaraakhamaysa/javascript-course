701c15cb7a9ff0c99360c56c92868b9505acd9f1
Baraa <engbaraakhamaysa@gmail.com>
2025-09-11 23:55:33 +0300

    Promise: Then, Catch & Finally  File

---
+++ 181_Promise_Then_Catch_Finally.js
@@ -0,0 +1,17 @@
+/* 
+
+Promise Training
+
+We Will Go To The Meeting, Promise Me That We Will Find The 4 Employees
+
+.then(We Will Chosse Tow People)
+.then(We Will Test Them Then Get One Of Them)
+.then(We Will Test Them Than Get One Of Them)
+
+them => Promise Is Successfull Use The Resolved Data
+Catch => Promise Is Failed, Catch The Error
+Finally => Promise Successfull Or Failed Finally Do Somethiing
+
+
+*/
+const myPromise = new Promise((resolveFunction, rejectFunction) => {});
